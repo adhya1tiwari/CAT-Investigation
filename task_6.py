3924d2d496b8e1fa0a68594aec0c43006b81af02
Adhya Tiwari <adhya.tiwari@gmail.com>
2025-04-14 17:23:33 +0800

    Monday 14th April Task 8 I have made a new function called step_any_loop. It takes an unlimited amount of arguments and loops through the list of 'args'. It goes in a constant loop until the shape is closed In line 31-34, if the turtle's position is back to the beginning position, the loop stops. And if the loop goes on forever and goes past the maximum iterations, the loop breaks.

--- task_6.py
+++ task_6.py
@@ -3,9 +3,11 @@ screen=turtle.Screen()
 screen.bgcolor("White")
 # create a turtle object to draw with
 pen=turtle.Turtle()
+
 # set colour of the turtle
 pen.speed()
 pen.color("Blue")
+
 #draw line
 pen.forward(20)
 pen.right(90)
@@ -14,6 +16,7 @@ pen.right(90)
 pen.forward(20)
 pen.right(90)
 pen.forward(30)
+
 #hide turtle
 pen.hideturtle()
 turtle.done()
--- task_7.py
+++ task_7.py
@@ -10,14 +10,33 @@ pen.color("Blue")
 pen.speed(2)
 
 # Define the step_3_loop function
-def step_3_loop(first, second, third, sequence):
-    list_move = [first, second, third]
-    for i in range(sequence):
-        for m in list_move:
-            pen.forward(m * 10)
+# def step_3_loop(first, second, third, sequence):
+#     list_move = [first, second, third]
+#     for i in range(sequence):
+#         for m in list_move:
+#             pen.forward(m * 10)
+#             pen.right(90)
+def step_any_loop(*args):
+    initial_position = pen.position()
+    initial_heading = pen.heading()
+    iteration = 0
+    maximum_iterations = 1000
+    while True:
+        for i in args:
+            pen.forward(i*10)
             pen.right(90)
+        iteration += 1
+        current_position = pen.position()
+        current_heading = pen.heading()
+        if (abs(current_position[0]-initial_position[0]) < 0.01 
+            and abs(current_position[1]-initial_position[1]) < 0.01 
+            and abs(current_heading-initial_heading) < 0.01):
+            break
+        if iteration == maximum_iterations:
+            break
 # Call the function
-step_3_loop(2, 4, 1, 6)
+step_any_loop(1,2,3,2,1)
+# step_3_loop(2, 4, 1, 6)
 
 # Hide the turtle and keep the window open
 pen.hideturtle()
